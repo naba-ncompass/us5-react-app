5bf63420e4bc33b64faa011dd42ecb4ded16acfa
naba-ncompass <naba@ncompass.in>
2022-08-08 12:37:02 +0530

    5 modification for demo

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -7,7 +7,7 @@ import './hero.css';
 const Hero = () => (
   <div className="hero">
     <h3 className="heading">
-      Robots, Now we can deploy and devlop any thing Naba v4.
+      Robots, Now we can deploy and devlop any thing Naba v5.
     </h3>
     <SectionLink targetSectionId="section-demo" />
   </div>
